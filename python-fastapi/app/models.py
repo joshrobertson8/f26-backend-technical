7ef65cdd75ffcc8338321df1b5b59a78bc969524
Josh Robertson <joshrobertson753@gmail.com>
2026-08-28 12:40:21 -0400

    Complete Python FastAPI models

--- python-fastapi/app/models.py
+++ python-fastapi/app/models.py
@@ -9,3 +9,11 @@ class User(BaseModel):
 class EventInput(BaseModel):
     model_config = {"extra": "forbid", "strict": True}
 
+    title: str
+    description: str = ""
+
+    inviteeIds: list[str] = []
+
+
+class Event(EventInput):
+    id: str
