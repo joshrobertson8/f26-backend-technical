30240d1e46919f25a21c1eb9096482a0667258c4
Josh Robertson <joshrobertson753@gmail.com>
2026-08-29 16:07:48 -0400

    Add create behavior to JavaScript Express controller

--- javascript-express/src/controller.js
+++ javascript-express/src/controller.js
@@ -9,3 +9,16 @@ export function controller(service, store) {
     response.json({ status: "ok" });
   });
 
+  router.get("/users", (request, response) => {
+    const users = Array.from(store.users.values());
+
+    response.json(users);
+  });
+
+  router.post("/events", (request, response) => {
+    const data = parseEventInput(request.body);
+    const event = service.create(data);
+
+    response.status(201).json(event);
+  });
+
