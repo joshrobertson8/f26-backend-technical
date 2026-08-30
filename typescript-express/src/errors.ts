de798c7a56b8bf7ba6516d66f8339e7b54fe8c85
Josh Robertson <joshrobertson753@gmail.com>
2026-08-30 14:52:32 -0400

    Complete TypeScript Express service errors

--- typescript-express/src/errors.ts
+++ typescript-express/src/errors.ts
@@ -4,3 +4,12 @@ export class ServiceError extends Error {
   constructor(status: number, message: string) {
     super(message);
 
+    this.status = status;
+  }
+}
+
+export class NotImplementedError extends ServiceError {
+  constructor() {
+    super(501, "Implement the event service");
+  }
+}
