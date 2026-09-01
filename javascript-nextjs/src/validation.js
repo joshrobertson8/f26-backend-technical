7c07790d62b51dc02d8fdc0840e6c8f2283dc240
Josh Robertson <joshrobertson753@gmail.com>
2026-09-01 08:55:36 -0400

    Add invitee handling to JavaScript Next.js request validation

--- javascript-nextjs/src/validation.js
+++ javascript-nextjs/src/validation.js
@@ -28,3 +28,13 @@ export function parseEventInput(body) {
     throw new ServiceError(400, "description must be a string");
   }
 
+  let invitees = body.inviteeIds;
+
+  if (invitees === undefined) {
+    invitees = [];
+  }
+
+  if (!Array.isArray(invitees)) {
+    throw new ServiceError(400, "inviteeIds must be a list");
+  }
+
