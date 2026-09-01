d7769df8e75195103a7fc8fa326ad57d38af63a4
Josh Robertson <joshrobertson753@gmail.com>
2026-09-01 11:53:24 -0400

    Complete JavaScript Next.js events API route

--- javascript-nextjs/src/app/api/events/route.js
+++ javascript-nextjs/src/app/api/events/route.js
@@ -3,3 +3,6 @@ import { create } from "../../../controller.js";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
+export async function POST(request) {
+  return create(request);
+}
