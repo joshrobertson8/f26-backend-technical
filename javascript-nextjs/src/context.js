3ae975c0d1d500856c8160be9cfb916002201c98
Josh Robertson <joshrobertson753@gmail.com>
2026-09-01 13:09:21 -0400

    Complete JavaScript Next.js context.js

--- javascript-nextjs/src/context.js
+++ javascript-nextjs/src/context.js
@@ -5,3 +5,5 @@ if (globalThis.eventStore === undefined) {
   globalThis.eventStore = new MemoryStore();
 }
 
+export const store = globalThis.eventStore;
+export const service = new EventService(store);
