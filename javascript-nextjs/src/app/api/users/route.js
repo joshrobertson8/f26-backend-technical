2c7f3a00dc1e80fc07b55240078807870e8a612b
Josh Robertson <joshrobertson753@gmail.com>
2026-09-01 12:40:08 -0400

    Complete JavaScript Next.js users API route

--- javascript-nextjs/src/app/api/users/route.js
+++ javascript-nextjs/src/app/api/users/route.js
@@ -3,3 +3,8 @@ import { store } from "../../../context.js";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
+export function GET() {
+  const users = Array.from(store.users.values());
+
+  return Response.json(users);
+}
