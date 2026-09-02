5e14057d7f6887698b54a446bb7c7f74829aadd7
Josh Robertson <joshrobertson753@gmail.com>
2026-09-02 13:17:16 -0400

    Add read behavior to TypeScript Next.js controller

--- typescript-nextjs/src/controller.ts
+++ typescript-nextjs/src/controller.ts
@@ -22,3 +22,25 @@ export async function create(request: Request) {
     const data = parseEventInput(body);
     const event = service.create(data);
 
+    return Response.json(event, { status: 201 });
+  } catch (error) {
+    return errorResponse(error);
+  }
+}
+
+export function read(eventId: string) {
+  try {
+    const event = service.read(eventId);
+
+    return Response.json(event);
+  } catch (error) {
+    return errorResponse(error);
+  }
+}
+
+export async function update(request: Request, eventId: string) {
+  try {
+    const body = await request.json();
+    const data = parseEventInput(body);
+    const event = service.update(eventId, data);
+
