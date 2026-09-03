8a2349ff8c9cbc9d7db14bc7ed8352cdc6c0b62a
Josh Robertson <joshrobertson753@gmail.com>
2026-09-03 14:55:12 -0400

    Add user handling to Java Spring Boot memory store

--- java-spring-boot/src/main/java/com/appteam/events/MemoryStore.java
+++ java-spring-boot/src/main/java/com/appteam/events/MemoryStore.java
@@ -5,3 +5,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+@Component
+public class MemoryStore {
+
+    public final Map<String, Event> events = new HashMap<>();
+    public final Map<String, User> users = new HashMap<>();
+
+    private int nextId = 1;
+
