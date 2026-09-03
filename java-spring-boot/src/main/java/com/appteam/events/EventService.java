417b4083756e887a7948b273d160a87641f4edd5
Josh Robertson <joshrobertson753@gmail.com>
2026-09-03 15:33:33 -0400

    Add create behavior to Java Spring Boot event service

--- java-spring-boot/src/main/java/com/appteam/events/EventService.java
+++ java-spring-boot/src/main/java/com/appteam/events/EventService.java
@@ -5,3 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventService {
 
+    private final MemoryStore store;
+
+    public EventService(MemoryStore store) {
+        this.store = store;
+    }
+
+    public Event create(EventInput data) {
+        throw new UnsupportedOperationException();
+    }
+
