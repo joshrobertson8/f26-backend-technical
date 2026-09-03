360d9cd215c7a92d989625af3a54427cde200163
Josh Robertson <joshrobertson753@gmail.com>
2026-09-03 14:24:05 -0400

    Complete Java Spring Boot service errors

--- java-spring-boot/src/main/java/com/appteam/events/ServiceException.java
+++ java-spring-boot/src/main/java/com/appteam/events/ServiceException.java
@@ -4,3 +4,13 @@ public class ServiceException extends RuntimeException {
 
     private final int status;
 
+    public ServiceException(int status, String message) {
+        super(message);
+
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
