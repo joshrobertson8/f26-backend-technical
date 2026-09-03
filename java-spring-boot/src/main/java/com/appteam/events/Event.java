766e3ef685e79ae287f5ba6e3eacd05c5d9023a4
Josh Robertson <joshrobertson753@gmail.com>
2026-09-03 12:40:59 -0400

    Complete Java Spring Boot event model

--- java-spring-boot/src/main/java/com/appteam/events/Event.java
+++ java-spring-boot/src/main/java/com/appteam/events/Event.java
@@ -3,3 +3,17 @@ package com.appteam.events;
 import java.util.ArrayList;
 import java.util.List;
 
+public class Event {
+
+    public String id;
+    public String title;
+    public String description;
+    public List<String> inviteeIds;
+
+    public Event(String id, String title, String description, List<String> inviteeIds) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.inviteeIds = new ArrayList<>(inviteeIds);
+    }
+}
