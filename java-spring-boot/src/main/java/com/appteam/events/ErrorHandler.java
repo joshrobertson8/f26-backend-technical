0aa34ebb112b60b347f332d2ece95688bea098b4
Josh Robertson <joshrobertson753@gmail.com>
2026-09-04 08:35:50 -0400

    Start Java Spring Boot error handling

---
+++ java-spring-boot/src/main/java/com/appteam/events/ErrorHandler.java
@@ -0,0 +1,10 @@
+package com.appteam.events;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
