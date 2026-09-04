c08b708f53d58003c364d5588619f2a0d9143e42
Josh Robertson <joshrobertson753@gmail.com>
2026-09-04 11:12:28 -0400

    Add build behavior to Java Spring Boot test launcher

--- java-spring-boot/test.py
+++ java-spring-boot/test.py
@@ -9,3 +9,20 @@ ROOT = Path(__file__).resolve().parent
 def main():
     print("Building Java project...", flush=True)
 
+    command = [
+        "java",
+        f"-Dmaven.multiModuleProjectDirectory={ROOT}",
+        "-classpath",
+        str(ROOT / ".mvn/wrapper/maven-wrapper.jar"),
+        "org.apache.maven.wrapper.MavenWrapperMain",
+        "-q",
+        "package",
+    ]
+
+    build = subprocess.run(command, cwd=ROOT, capture_output=True, text=True)
+
+    if build.returncode != 0:
+        print("\nRESULT: BUILD FAILED")
+        print(build.stdout)
+        print(build.stderr)
+
