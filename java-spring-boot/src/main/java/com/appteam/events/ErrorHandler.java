8fbed2c9f1fdce3874cb8fff49cfd824d6b25c38
Josh Robertson <joshrobertson753@gmail.com>
2026-09-04 08:52:52 -0400

    Add error handling to Java Spring Boot error handling

--- java-spring-boot/src/main/java/com/appteam/events/ErrorHandler.java
+++ java-spring-boot/src/main/java/com/appteam/events/ErrorHandler.java
@@ -8,3 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ErrorResponse> service(ServiceException error) {
+        ErrorResponse body = new ErrorResponse(error.getMessage());
+
+        return ResponseEntity.status(error.getStatus()).body(body);
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public ResponseEntity<ErrorResponse> unfinished() {
+        ErrorResponse body = new ErrorResponse("Implement the event service");
+
