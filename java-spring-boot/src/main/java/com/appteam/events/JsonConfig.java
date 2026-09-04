8f90293a032d9d6976c63fae8b8aa3935ae3e895
Josh Robertson <joshrobertson753@gmail.com>
2026-09-04 10:13:55 -0400

    Add JSON handling to Java Spring Boot JSON configuration

--- java-spring-boot/src/main/java/com/appteam/events/JsonConfig.java
+++ java-spring-boot/src/main/java/com/appteam/events/JsonConfig.java
@@ -10,3 +10,13 @@ import com.fasterxml.jackson.databind.type.LogicalType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
+
