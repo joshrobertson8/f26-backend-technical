c0b695826b396a7efcf727fa0247753c4275463b
Josh Robertson <joshrobertson753@gmail.com>
2026-09-05 12:11:29 -0400

    Complete C event service

--- c/src/service.c
+++ c/src/service.c
@@ -4,3 +4,15 @@ ServiceStatus service_create(MemoryStore *store, const EventInput *data, const E
     return SERVICE_NOT_IMPLEMENTED;
 }
 
+ServiceStatus service_read(MemoryStore *store, const char *event_id, const Event **out) {
+    return SERVICE_NOT_IMPLEMENTED;
+}
+
+ServiceStatus service_update(MemoryStore *store, const char *event_id, const EventInput *data,
+                            const Event **out) {
+    return SERVICE_NOT_IMPLEMENTED;
+}
+
+ServiceStatus service_delete(MemoryStore *store, const char *event_id) {
+    return SERVICE_NOT_IMPLEMENTED;
+}
