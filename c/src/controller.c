e85657d95793371cfb78c97d09933a4f246564cd
Josh Robertson <joshrobertson753@gmail.com>
2026-09-05 12:56:09 -0400

    Start C controller

---
+++ c/src/controller.c
@@ -0,0 +1,35 @@
+#include "controller.h"
+#include "service.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+HttpResponse http_error(int status) {
+    const char *message = "Internal server error";
+
+    switch (status) {
+        case 400:
+            message = "Invalid event or invitations";
+            break;
+        case 404:
+            message = "Not found";
+            break;
+        case 405:
+            message = "Method not allowed";
+            break;
+        case 413:
+            message = "Request too large";
+            break;
+        case 501:
+            message = "Implement the event service";
+            break;
+    }
+
+    cJSON *json = cJSON_CreateObject();
+    cJSON_AddStringToObject(json, "error", message);
+
+    char *body = cJSON_PrintUnformatted(json);
+    cJSON_Delete(json);
+
+    HttpResponse response = {status, body};
+
