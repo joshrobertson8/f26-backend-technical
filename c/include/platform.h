20b21adc7884028977b78e53b4d5f5a4453f6823
Josh Robertson <joshrobertson753@gmail.com>
2026-09-06 09:25:57 -0400

    Add validation to C platform helpers

--- c/include/platform.h
+++ c/include/platform.h
@@ -14,3 +14,18 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#define strncasecmp _strnicmp
+
+typedef SOCKET Socket;
+#define INVALID_CONNECTION INVALID_SOCKET
+
+static bool start_sockets(void) {
+    WSADATA data;
+
+    return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+}
+
+static void stop_sockets(void) {
+    WSACleanup();
+}
+
